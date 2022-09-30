e32e90c2514a5c29cdf49a327b58a2da78c6a204
Nedim AKCA <nedimakca87@hotmail.com>
2022-09-30 13:43:30 +0300

    Şart Blokları

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,16 +10,42 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var number = 10;
-            if (number==10)
+            var number = 11;
+
+            //if ile şart bloğu
+            if (number == 10)
             {
                 Console.WriteLine("Number is 10");
             }
+            else if (number == 20)
+            {
+                Console.WriteLine("Number is 20");
+            }
             else
             {
-                Console.WriteLine("Number is not 10");
+                Console.WriteLine("Number is not 10 or 20");
             }
 
+
+            //if ile tek satır şart bloğu
+            Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");    // Tek Satır if bloğu örneği.
+
+
+            //switch ile şart bloğu
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10 or 20");
+                    break;
+            }
+
+            //konsolun açık kalması için
             Console.ReadLine();
         }
     }
