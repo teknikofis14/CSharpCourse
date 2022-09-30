64a821025151fb9680086e0655750aa6e509cbe9
Nedim AKCA <nedimakca87@hotmail.com>
2022-09-30 11:57:04 +0300

    Şart Blokları

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,17 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            var number = 10;
+            if (number==10)
+            {
+                Console.WriteLine("Number is 10");
+            }
+            else
+            {
+                Console.WriteLine("Number is not 10");
+            }
+
+            Console.ReadLine();
         }
     }
 }
