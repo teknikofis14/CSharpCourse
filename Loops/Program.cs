15fdd0ca64b816787a2753fd3b10af0529044625
Nedim AKCA <nedimakca87@hotmail.com>
2022-10-03 09:06:47 +0300

    Döngüler

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,6 +10,27 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            //for döngüsü
+            //1'den 100'e kadar olan sayıları döndürür.
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            //while döngüsü
+            int number = 100;
+            while (number>=0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+
+
+
+            //Konsolun açık kalmasını sağlar.
+            Console.ReadLine();
         }
     }
 }
