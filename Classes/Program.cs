c2920e35dcb39d48ba6ff11c9969834963a4baab
Nedim AKCA <nedimakca87@hotmail.com>
2022-10-03 14:42:18 +0300

    Sınıflarla Çalışmak

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,7 +10,34 @@ namespace Classes
     {
         static void Main(string[] args)
         {
+            //Class oluşturulurken ilk harfi büyük yazılır. Ama onun örneği yazılırken ilk harfi küçük yazılır. CustomerManager Class, customerManager onun örneğidir.
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
 
+
+
+
+
+
+
+            //Konsol ekranında bekletir.
+            Console.ReadLine();
+        }
+
+    }
+
+
+    class CustomerManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated!");
         }
     }
 }
