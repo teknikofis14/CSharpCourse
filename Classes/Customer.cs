70f3824250489674fe6b792764f72b30aa71cf64
Nedim AKCA <nedimakca87@hotmail.com>
2022-10-03 16:00:16 +0300

    Sınıflarla Çalışmak

---
+++ Classes/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Customer
+    {
+        //prop yazıp iki kere Tab bastık public ... ... {get; set} otomatik ekledi. property kısaltması
+        //Müşterinin özelliklerini tutmak için ekledik.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -23,6 +23,28 @@ namespace Classes
             productManager.Update();
 
 
+            //Customer sınıfını çağırdım customer örneğine atadım ve Id, FirstName gibi değerleri atadım.
+            //Customer sınıfındaki set bloğunu çalıştırır.
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Nedim";
+            customer.LastName = "AKCA";
+            customer.City = "Karabük";
+
+
+            //Bir üstteki sınıf çağırmayla aynı işlevi görür. Parantez aç kapa yerine süslü parantez açıp değerleri atadım.
+            Customer customer2 = new Customer
+            {
+                //Ctrl+Spacebar basınca Customer sınıfında tanımlanan değerler (Id, FirstName vb.) çıkıyor.
+                Id = 2, FirstName = "Emre", LastName = "AKCA", City = "Karabük"
+            };
+
+
+            //customer2'nin FirstName değerini ekrana yazdırır.
+            //Customer sınıfındaki get bloğunu çalıştırır.
+            Console.WriteLine(customer2.FirstName);
+
+
             //Konsol ekranında bekletir.
             Console.ReadLine();
         }
