cc63027456e749765df45ee6cad51e06b13f00b6
Nedim AKCA <nedimakca87@hotmail.com>
2022-10-04 17:05:35 +0300

    Interface ile Çalışmak - Çoklu İmplementasyon

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+
+    interface IWorker
+    {
+        void Work();
+        void Eat();
+        void GetSallary();
+    }
+
+
+
+    class Manager : IWorker
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSallary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    class Worker : IWorker
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSallary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    class Robot : IWorker
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSallary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+
+
+
+}
